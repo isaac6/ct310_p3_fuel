6bd02685cf2c40b0c75b06f9d1183bbc93ad68ed
isaac6 <isaac.hall@colostate.edu>
2018-04-25 10:28:02 -0600

    Added state to attraction table creation and ORM model

--- classes/model/ormattraction.php
+++ classes/model/ormattraction.php
@@ -4,7 +4,7 @@ namespace Model;
 
 class Ormattraction extends \Orm\Model
 {
-    protected static $_properties = array('attractionID', 'name', 'details', 'img');
+    protected static $_properties = array('attractionID', 'name', 'state', 'details', 'img');
     protected static $_table_name = 'attractions';
     protected static $_primary_key = array('attractionID');
 }
