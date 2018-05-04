53929459c364e1ffaa984a7fce4515f9e968a3cc
isaac6 <isaac.hall@colostate.edu>
2018-05-04 14:28:25 -0600

    external attractions

---
+++ views/federation/view_external_attraction.php
@@ -0,0 +1,8 @@
+<div id="content" class="container-fluid dark-opac-bg">
+  <div id="attraction">
+    <center><h2><?=$name?></h2></center>
+    <center><img src="/~<?=$eid;?>/ct310/index.php/federation/attrimage/<?=$id;?>"></center>
+    <center><h5><?=$state?></h5></center>
+    <center><h5><?=$details?></h5></center>
+  </div>
+</div>
